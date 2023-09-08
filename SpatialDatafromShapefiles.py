a60109d4a6f85b06fbe40a9a75bddbf7b8b4b45a
abbycriswell <abbylcriswell@gmail.com>
2023-09-07 20:34:34 -0500

    combined range calculation with union and provided default range type lists

--- SpatialDatafromShapefiles.py
+++ SpatialDatafromShapefiles.py
@@ -1,29 +1,45 @@
-# Species Shapes
 # made by Abby Criswell
-# with the Havird Lab at
-# the University of Texas at Austin
-# updated 5-4-2023
+# with the University of Texas at Austin
+# updated 9-7-2023
 
 import csv
 from os import listdir
 from os.path import isdir, join, splitext
 import geopandas
 
-#Run:
+#To Run:
 # run in command line: python SpatialDatafromShapefiles.py
-# after changing path variables!
+# after changing path variables (path, shapefile_path, specieslist_path)
+# and range types of interest list variable (range_types_of_interest)
 
-#CHANGE THESE:
+#PATH VARIABLES:
+#CHANGE THESE AS NEEDED
 #path: where output should be put
-#shapefile_path: folder containing shapefile folders (labeled by species)
-#specieslist_path: path to csv file with list of species to try
-
 path="./Example/"
+#shapefile_path: folder containing shapefile folders (labeled by species)
 shapefile_path = "./Example/" + "Data_Folder/"
+#specieslist_path: path to csv file with list of species to try
 specieslist_path = "./Example/" + "specieslist.csv"
+
+#RANGE TYPE GROUPS:
+#all: all range types
+all_range_types = ["extant (resident)", "extant (breeding)", "extant (non-breeding)", "extant (passage)", "probably extant (resident)", "possibly extant (resident)", "extant & origin uncertain (resident)", "extant & reintroduced (resident)", "extant & introduced (resident)", "extant & vagrant (seasonality uncertain)", "probably extant & origin uncertain (resident)", "possibly extant & vagrant (seasonality uncertain)", "presence uncertain", "possibly extinct & introduced", "possibly extinct", "extinct & reintroduced", "extinct"]
+#historical: extant and extinct resident and breeding areas (includes reintroducted and origin uncertain, but not introduced, non-breeding, vagrant, or passage)
+historical_range_types = ["extant (resident)", "extant (breeding)", "probably extant (resident)", "possibly extant (resident)", "extant & origin uncertain (resident)", "extant & reintroduced (resident)", "probably extant & origin uncertain (resident)", "presence uncertain", "possibly extinct", "extinct & reintroduced", "extinct"]
+#current: extant resident and breeding areas (includes introduced, reintroduced, possibly extinct, and origin uncertain, but not extinct, non-breeding, vagrant, or passage)
+current_range_types = ["extant (resident)", "extant (breeding)", "probably extant (resident)", "possibly extant (resident)", "extant & origin uncertain (resident)", "extant & reintroduced (resident)", "extant & introduced (resident)", "probably extant & origin uncertain (resident)", "presence uncertain", "possibly extinct", "possibly extinct & introduced", "extinct & reintroduced"]
+#custom: make your own list of range types to include in the area calcs
+custom_range_types = []
+
+#SELECT RANGE TYPE LIST TO USE:
+#CHANGE THIS AS NEEDED (use default lists provided or custom list)
+#defaults to historical range group
+area_types_of_interest = historical_range_types
+
 
 print("Starting...\nPath: ", path)
 
+#get list of species to process
 lookingfor_species = []
 with open(specieslist_path) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
@@ -34,39 +50,42 @@ with open(specieslist_path) as csv_file:
         else:
             lookingfor_species.append(row[0])
 
-'''
-print("Looking for information on:\n")
-for species in lookingfor_species:
-    print(species, "\n")
-'''
-
+#check if we have shapefile folders named for each species in list
 haveinfofor_species = [f for f in listdir(shapefile_path) if isdir(join(shapefile_path, f))]
-'''
-#DEPRICATED (moved from files to folders)
-haveinfofor_species = []
-for filename in haveinfofor_speciesdirs:
-    haveinfofor_species.append(splitext(filename)[0])
-'''
 
-info = {}
-donthaveinfofor_species = []
+#set up species_info output file
 species_count = 0
+info_file = open(path + "species_info.tsv", "w")
+info_file.write("Species\tTotal Area (sq km)\t+/-\tMin Lat\t+/-\tMax Lat\t# Ranges Included\t# Ranges Excluded (Introduced)\n")
+
+#set up species_notfound output file
+donthaveinfofor_species = []
 species_notfound_count = 0
+donthaveinfofor_file = open(path + "species_notfound.txt", "w")
+
+#try to calculate area for each species
 for species in lookingfor_species:
     if species in haveinfofor_species:
-        species_count += 1
-        info[species] = [0 for i in range(3)]
+
+        #open species' shapefile
         species_range_data = geopandas.read_file(shapefile_path + species + "/data_0.shp")
 
-        species_range_bounds = species_range_data.bounds
+        #only keep ranges of types we're interested in
+        for index, entry in species_range_data.iterrows():
+            label = entry["LEGEND"].lower()
+            if label not in area_types_of_interest:
+                species_range_data.drop(index, inplace=True)
+                print(label)
 
+        #find bounds of range
+        species_range_bounds = species_range_data.bounds
         south_lat = 90
         north_lat = -90
         for index, entry in species_range_bounds.iterrows():
-                if float(entry[1]) < south_lat:
-                    south_lat = float(entry[1])
-                if float(entry[3]) > north_lat:
-                    north_lat = float(entry[3])
+            if float(entry[1]) < south_lat:
+                south_lat = float(entry[1])
+            if float(entry[3]) > north_lat:
+                north_lat = float(entry[3])
 
         if abs(south_lat) > abs(north_lat):
             max_lat = south_lat
@@ -75,36 +94,47 @@ for species in lookingfor_species:
             max_lat = north_lat
             min_lat = south_lat
 
-        info[species][1] = min_lat
-        info[species][2] = max_lat
+        #separately record signs for later processing
+        if min_lat < 0: min_sign = "-"
+        else: min_sign = "+"
+        if max_lat < 0: max_sign = "-"
+        else: max_sign = "+"
 
-        species_range_cartesian = species_range_data.to_crs(3857)
+        #find area of range
+        #Eckert IV projection (unit: m) (can't get area from lat/lon coordinates)
+        species_range_cartesian = species_range_data.to_crs("+proj=eck4 +lon_0=0 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs")
 
-        area_total = 0
-        for index, entry in species_range_cartesian.iterrows():
-            area_total += entry["geometry"].area / 10**6
+        #combined range calculations attempt to do a union on all ranges (dissolve()) to account for overlap
+        #but may encounter errors due to how geopandas library handles range border conflicts
+        #if we encounter an error, we do not report information for that species
+        try:
+            species_combined_range = species_range_cartesian.dissolve()
+            if species_combined_range.shape[0] != 1:
+                print("dissolve resulted in multiple entries")
+                area_total = -1
+            else:
+                for index, entry in species_combined_range.iterrows():
+                    #convert from sq m to sq km
+                    area_total = entry["geometry"].area / 10**6
+        except:
+            print("Error with unary union")
+            area_total = -1
 
-        area_avg = area_total / len(species_range_cartesian)
-        info[species][0] = area_avg
+        if area_total == -1:
+            #handle area calculation error cases
+            species_notfound_count += 1
+            donthaveinfofor_file.write(species + "\n")
+        else:
+            #record species info
+            info = [species, str(area_total), min_sign, str(min_lat), max_sign, str(max_lat), str(og_count), str(introduced_count)]
+            info_file.write('\t'.join(info) + "\n")
 
     else:
+        #error finding shapefile folder
         species_notfound_count += 1
-        donthaveinfofor_species.append(species)
+        donthaveinfofor_file.write(species + "\n")
 
-
-info_file = open(path + "species_info.tsv", "w")
-info_file.write("Species\tArea (sq km)\t+/-\tMin Lat\t+/-\tMax Lat\n")
-for species in info:
-    if info[species][1] < 0: min_sign = "-"
-    else: min_sign = "+"
-    if info[species][2] < 0: max_sign = "-"
-    else: max_sign = "+"
-    info_file.write(species + "\t" + str(info[species][0]) + "\t" + min_sign + "\t" + str(info[species][1]) + "\t" + max_sign + "\t" + str(info[species][2])+"\n")
 info_file.close()
-
-donthaveinfofor_file = open(path + "species_notfound.txt", "w")
-for species in donthaveinfofor_species:
-    donthaveinfofor_file.write(species + "\n")
 donthaveinfofor_file.close()
 
 print("Done! Processed ", species_count, " species' shapefiles. ", species_notfound_count, "species' shapefiles could not be found.\nOutput:\n- Species info output to 'path\species_info.tsv'. \n- Any species that did not have a shapefile are listed in 'path\species_notfound.txt'.")
